d2e25ea53c18260fd6f545c621818d9dc74789aa
Chris Allick <chrisallick@gmail.com>
2015-01-30 11:26:17 -0500

    removed unused files, cleaned up folder structure, got js executing with debugging

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+var t = 10;
 $(document).ready(function() {
+    // receive messages
+    // chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    //     if( request.type == "msg" ) {
+    //         console.log( request.msg );
+    //     }
+    // });
 
+    // send message
+    // addEventListener("unload", function (event) {
+    //     chrome.runtime.sendMessage({type:"pause"},function(response) {});
+    // }, true);
+
+    // chrome.storage.local.set({'didSubmit_t': "false"}, function (result) {
+
+    // });
+
+    chrome.storage.sync.set({'rand_value': "coo"}, function() {
+      	console.log("ASDFs");
+    });
+
+    console.log(t);
 });
